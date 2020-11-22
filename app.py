2d2775d60ca525f380ff3e863be491fb17dd5e38
UltraViolet5 <70701763+UltraViolet5@users.noreply.github.com>
2020-11-22 11:17:35 +0100

    commit

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import user_story
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    user_stories = user_story.get_user_stories()
+    headers = user_story.get_headers()
+    return render_template("index.html", stories=user_stories, headers=headers)
 
 
 if __name__ == '__main__':
